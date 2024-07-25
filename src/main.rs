b8f4ab0e2eb8a394d6d647a668f56f95fb44e56d
lethuzulu <lethuzulu345@gmail.com>
2024-07-26 01:21:50 +0200

    step1 add byte counter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env::args;
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    //1. get command line arguments
+
+    let arguments: Vec<String> = args().collect();
+    let file_string = &arguments[1];
+
+    let file_path = Path::new(file_string);
+
+    let mut file = OpenOptions::new().read(true).open(file_path).unwrap();
+
+    let mut temp_buf: Vec<u8> = vec![];
+
+    let num_bytes = file.read_to_end(&mut temp_buf).unwrap();
+
+     println!("{:?}", num_bytes);
 }
